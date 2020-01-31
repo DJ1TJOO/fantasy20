10d34b9f963d7176a9a029bb31b32b2226130ded
DJ1TJOO <t.ferb1@gmail.com>
2020-01-31 21:54:25 +0100

    Conditions question

---
+++ src/main/java/nl/fantasynetworkmc/fantasy20/recipe/conditions/ResearchedCondition.java
@@ -0,0 +1,34 @@
+package nl.fantasynetworkmc.fantasy20.recipe.conditions;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.conditions.ICondition;
+import nl.fantasynetworkmc.fantasy20.Fantasy20;
+
+public class ResearchedCondition implements ICondition {
+
+    private static final ResourceLocation NAME = new ResourceLocation(Fantasy20.MODID, "researched_condition");
+    private final ResourceLocation item;
+
+    public ResearchedCondition(String location) {
+        this(new ResourceLocation(location));
+    }
+
+    public ResearchedCondition(String namespace, String path) {
+        this(new ResourceLocation(namespace, path));
+    }
+
+    public ResearchedCondition(ResourceLocation item) {
+        this.item = item;
+    }
+    
+	@Override
+	public ResourceLocation getID() {
+		return NAME;
+	}
+
+	@Override
+	public boolean test() {
+		return false;
+	}
+
+}
