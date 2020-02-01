66641bdf86b63aa3a8d278baf1a88bd9e9416267
DJ1TJOO <t.ferb1@gmail.com>
2020-02-01 13:18:09 +0100

    RegisterAll

--- src/main/java/nl/fantasynetworkmc/fantasy20/setup/RegistryEvents.java
+++ src/main/java/nl/fantasynetworkmc/fantasy20/setup/RegistryEvents.java
@@ -35,47 +35,51 @@ public class RegistryEvents {
 	
     @SubscribeEvent
     public static void onBlocksRegistry(final RegistryEvent.Register<Block> e) {
-       e.getRegistry().register(new ResearchTable());
-       e.getRegistry().register(new DoorLock());
-       e.getRegistry().register(new FloorFrame());
-       e.getRegistry().register(new WallFrame());
-       e.getRegistry().register(new MetalOre());
+       e.getRegistry().registerAll(
+    		   new ResearchTable(),
+    		   new DoorLock(),
+    		   new FloorFrame(),
+    		   new WallFrame(),
+    		   new MetalOre());
     }
     @SubscribeEvent
     public static void onItemsRegistry(final RegistryEvent.Register<Item> e) {
     	Item.Properties properties = new Item.Properties();
     	properties.group(ModSetup.itemGroup);
-       e.getRegistry().register(new BlockItem(ModBlocks.RESEARCH_TABLE, properties).setRegistryName("fantasy20:research_table"));
-       e.getRegistry().register(new BlockItem(ModBlocks.DOORLOCK, properties).setRegistryName("fantasy20:doorlock"));
-       e.getRegistry().register(new BlockItem(ModBlocks.FLOOR_FRAME, properties).setRegistryName("fantasy20:floor_frame"));
-       e.getRegistry().register(new BlockItem(ModBlocks.WALL_FRAME, properties).setRegistryName("fantasy20:wall_frame"));
-       e.getRegistry().register(new BlockItem(ModBlocks.METAL_ORE, properties).setRegistryName("fantasy20:metal_ore"));
-       e.getRegistry().register(new Blueprint(properties));
-       e.getRegistry().register(new Metal(properties));
-       e.getRegistry().register(new WoodenPanel(properties));
-       e.getRegistry().register(new StonePanel(properties));
-       e.getRegistry().register(new MetalPanel(properties));
-       e.getRegistry().register(new Scrap(properties));
+    	e.getRegistry().registerAll(
+    		new BlockItem(ModBlocks.RESEARCH_TABLE, properties).setRegistryName("fantasy20:research_table"),
+    		new BlockItem(ModBlocks.DOORLOCK, properties).setRegistryName("fantasy20:doorlock"),
+    		new BlockItem(ModBlocks.FLOOR_FRAME, properties).setRegistryName("fantasy20:floor_frame"),
+    		new BlockItem(ModBlocks.WALL_FRAME, properties).setRegistryName("fantasy20:wall_frame"),
+    		new BlockItem(ModBlocks.METAL_ORE, properties).setRegistryName("fantasy20:metal_ore"),
+    		new Blueprint(properties),
+    		new Metal(properties),
+    		new WoodenPanel(properties),
+    		new StonePanel(properties),
+    		new MetalPanel(properties),
+    		new Scrap(properties));
     }
     
     @SubscribeEvent
     public static void onTileEntityRegistry(final RegistryEvent.Register<TileEntityType<?>> e) {
-       e.getRegistry().register(TileEntityType.Builder.create(ResearchTableTile::new, ModBlocks.RESEARCH_TABLE).build(null).setRegistryName("fantasy20:research_table"));
-       e.getRegistry().register(TileEntityType.Builder.create(DoorLockTile::new, ModBlocks.DOORLOCK).build(null).setRegistryName("fantasy20:doorlock"));
-       e.getRegistry().register(TileEntityType.Builder.create(FloorFrameTile::new, ModBlocks.FLOOR_FRAME).build(null).setRegistryName("fantasy20:floor_frame"));
-       e.getRegistry().register(TileEntityType.Builder.create(WallFrameTile::new, ModBlocks.WALL_FRAME).build(null).setRegistryName("fantasy20:wall_frame"));
+       e.getRegistry().registerAll(
+    		   TileEntityType.Builder.create(ResearchTableTile::new, ModBlocks.RESEARCH_TABLE).build(null).setRegistryName("fantasy20:research_table"),
+    		   TileEntityType.Builder.create(DoorLockTile::new, ModBlocks.DOORLOCK).build(null).setRegistryName("fantasy20:doorlock"),
+    		   TileEntityType.Builder.create(FloorFrameTile::new, ModBlocks.FLOOR_FRAME).build(null).setRegistryName("fantasy20:floor_frame"),
+    		   TileEntityType.Builder.create(WallFrameTile::new, ModBlocks.WALL_FRAME).build(null).setRegistryName("fantasy20:wall_frame"));
     }
     
     @SubscribeEvent
     public static void onContainerRegistry(final RegistryEvent.Register<ContainerType<?>> e) {
-       e.getRegistry().register(IForgeContainerType.create((windowId, inv, data) -> {
-    	   BlockPos pos = data.readBlockPos();
-    	   return new ResearchTableContainer(windowId, Fantasy20.proxy.getClientWorld(), pos, inv, Fantasy20.proxy.getClientPlayer());
-       }).setRegistryName("fantasy20:research_table"));
-       e.getRegistry().register(IForgeContainerType.create((windowId, inv, data) -> {
-    	   BlockPos pos = data.readBlockPos();
-    	   return new DoorLockContainer(windowId, Fantasy20.proxy.getClientWorld(), pos, inv, Fantasy20.proxy.getClientPlayer());
-       }).setRegistryName("fantasy20:doorlock"));
+    	e.getRegistry().registerAll(
+    			IForgeContainerType.create((windowId, inv, data) -> {
+    				BlockPos pos = data.readBlockPos();
+    				return new ResearchTableContainer(windowId, Fantasy20.proxy.getClientWorld(), pos, inv, Fantasy20.proxy.getClientPlayer());
+    			}).setRegistryName("fantasy20:research_table"),
+    			IForgeContainerType.create((windowId, inv, data) -> {
+    				BlockPos pos = data.readBlockPos();
+    				return new DoorLockContainer(windowId, Fantasy20.proxy.getClientWorld(), pos, inv, Fantasy20.proxy.getClientPlayer());
+    			}).setRegistryName("fantasy20:doorlock"));
     }
     
 }
